43477530820918438f57bb545e235fd988979249
Adarsh Prajapati <154526910+adarshhdeveloper@users.noreply.github.com>
2026-09-10 04:11:21 +0000

    Created transaction model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+
+const transactionSchema = new mongoose.Schema({
+
+    fromAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"account",
+        required:[true,"Transaction must be associated with a from account."],
+        index:true
+    },
+    toAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"account",
+        required:[true,"Transaction must be associated with a to account."],
+        index:true
+    }
+    status:{
+        type: String,
+        enum:{
+            values:["PENDING","COMPLETED","FAILDE","REVERSED"],
+            message:"Status can be either PENDING , COMPLETED , FAILED or REVERSED",
+        },
+        default:"PENDING"    
+    },
+    amount:{
+        type:Number,
+        required : [true,"Amount is required for creating a transaction "],
+        min :[0,"Transaction amount cannot be negative"]
+    },
+    idempotencyKey:{
+        type:String,
+        require:[true,"Ideompotency key is required for creating a transaction"],
+        index:true,
+        unique:true,
+        comment:"Ye same payment ko 2 bar karne se rokti hii , hamesa client side se generate hoti hii ek transaction ke liye ek hi key hogi alway unique hoti hi " 
+    }
+},{
+    timestamps:true
+})
+
+const transactionModel = mongoose.model("transaction,transactionSchema")
+
+module.exports = transactionModel
