63b011804289b0d822033cffefa356d8c5e0369d
Adarsh Prajapati <code.adarsh121@gmail.com>
2026-09-11 13:37:44 +0530

    Created transaction API

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -44,10 +44,67 @@ async function createTranseaction(req, res) {
         _id: toAccount
     })
 
-    if(!fromUserAccount || !toUserAccount){
+    if (!fromUserAccount || !toUserAccount) {
         return res.status(400).json({
-            message:"Invalid fromAccount or toAccount"
+            message: "Invalid fromAccount or toAccount"
         })
     }
-    
+
+    /**
+     * -Validate idempotency Key 
+     */
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey: idempotencyKey
+    })
+
+    if (isTransactionAlreadyExists) {
+        if (isTransactionAlreadyExists.status === "COMPLETED") {
+            return response.status(200).json({
+                message: "Transaction is already processed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "PENDING") {
+            return res.status(200).json({
+                message: "Transaction is still processing"
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "FAILED") {
+            return res.status(500).json({
+                message: "Transaction processing failed , please retry ."
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "REVERSED") {
+            return res.status(500).json({
+                message: "Transaction was reversed , please retry"
+            })
+        }
+    }
+
+    /**
+     * -3 .Check account status
+     */
+
+    if(fromUserAccount.status !=="ACTIVE" || toUserAccount.status !== "ACTIVE"){
+        return res.status(400).json({
+            message : "Both fromUserAccount and toUserAccount must be ACTIVE to process transaction ." 
+        })
+    }
+
+    /**
+     * -4. Derive sender balance from ledger
+     */
+
+    const balance = await fromUserAccount.getBalance()
+
+    if(balance < amount){
+        return res.status(400).json({
+             message: `Insufficient balance. Current balance is ${balance}. Requested amount is ${amount}`
+        })
+    }
+
 }
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -31,6 +31,62 @@ accountSchema.index({
     status: 1
 })
 
+//function for fetch balance *****************************************************************
+accountSchema.method.getBlance = async function() {
+    accountSchema.methods.getBalance = async function() {
+
+        const balanceData = await ledgerModel.aggregate([{
+                $match: {
+                    account: this._id
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalDebit: {
+                        $sum: {
+                            $cond: [{
+                                    $eq: ["$type", "DEBIT"]
+                                },
+                                "$amount",
+                                0
+                            ]
+                        }
+                    },
+                    totalCredit: {
+                        $sum: {
+                            $cond: [{
+                                    $eq: ["$type", "CREDIT"]
+                                },
+                                "$amount",
+                                0
+                            ]
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    balance: {
+                        $subtract: ["$totalCredit", "$totalDebit"]
+                    }
+                }
+            }
+        ])
+
+        if (balanceData.length === 0) {
+            return 0
+        }
+
+        return balanceData[0].balance
+
+    }
+
+
+}
+
+
 const accountModel = mongoose.model("account", accountSchema)
 
 module.exports = accountModel
